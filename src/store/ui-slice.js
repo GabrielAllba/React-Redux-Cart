92e1313fa14cb2e3015fe8e4df06ffbf15ac87d8
rielgbrl <rielgasy9@gmail.com>
2023-01-18 10:36:43 +0700

    first commit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,14 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+createSlice({
+    name: 'ui',
+    initialState: {
+        cartIsVisible: false
+    },
+    reducers: {
+        toggle(state){
+            state.cartIsVisible = !state.cartIsVisible
+        }
+    }
+
+})
