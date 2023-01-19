8e92908d1ffd54a3e4bef8b37c110223d3c6e9f1
rielgbrl <rielgasy9@gmail.com>
2023-01-19 10:46:16 +0700

    using action creator thunk

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useSelector, useDispatch} from 'react-redux';
 import { useEffect} from 'react';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitital = true;
 
@@ -17,48 +18,50 @@ function App() {
   const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
+    dispatch(fetchCartData())
+  },[dispatch])
 
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data'
-      }))
+  useEffect(() => {
 
-      const response = await fetch("https://react-cart-d0778-default-rtdb.firebaseio.com/cart.json", {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
+    // const sendCartData = async () => {
+      // dispatch(uiActions.showNotification({
+      //   status: 'pending',
+      //   title: 'Sending...',
+      //   message: 'Sending cart data'
+      // }))
 
-      if(!response.ok){
-        throw new Error('Sending cart data failed!')
-      }
+      // const response = await fetch("https://react-cart-d0778-default-rtdb.firebaseio.com/cart.json", {
+      //   method: "PUT",
+      //   body: JSON.stringify(cart),
+      // });
 
-      dispatch(
-        uiActions.showNotification({
-          status: "Success",
-          title: "Success...",
-          message: "Sent cart data successfully",
-        })
-      );
+      // if(!response.ok){
+      //   throw new Error('Sending cart data failed!')
+      // }
 
-      const responseData = await response.json()
-    }
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "Success",
+      //     title: "Success...",
+      //     message: "Sent cart data successfully",
+      //   })
+      // );
+
+      // const responseData = await response.json()
+    // }
 
     if(isInitital){
       isInitital=false;
       return
     }
 
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error...",
-          message: "Sending cart data error",
-        })
-      );
-    })
+    if(cart.changed){
+      dispatch(sendCartData(cart))
+    }
+
+    // sendCartData().catch(error => {
+      
+    // })
   },[cart, dispatch])
 
   return (
